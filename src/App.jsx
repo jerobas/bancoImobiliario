34f9965b95d01ec57467b18ea694e7b22f06e223
flaviozno <flavio.vezono@ufu.br>
2023-05-17 20:18:51 -0300

    themes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify'
 
 import { useAuth0 } from '@auth0/auth0-react'
+import { ThemeProvider } from 'styled-components'
 
 import store from './pages/Redux/index.js'
 import Routes from './Routes/Routes';
 import GlobalStyles from './styles/global.styles';
+import { globalTheme } from './styles/theme/global.theme';
+
 function App() {
   const { isLoading, error } = useAuth0()
   return (
     <Provider store={store}>
+      <ThemeProvider theme={globalTheme}>
       {error && <p>Authentication Error</p>}
       {
         !error && !isLoading && (
@@ -33,7 +37,7 @@ function App() {
           </>
         )
       }
-
+  </ThemeProvider>
     </Provider>
   )
 }

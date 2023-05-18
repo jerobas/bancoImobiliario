2622fdb4355078ef856da1db1916e37222badfe1
João Vitor Sanches <desenvolvimento15@sifat.com.br>
2023-05-18 04:47:13 -0300

    starting room list page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,13 +6,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import Board from '../pages/Board/Board';
 import Login from '../pages/Login/Login';
+import Rooms from '../pages/Rooms/Rooms';
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const isAuthenticatedLocal = useSelector(state => state.auth.isAuthenticatedLocal);
-    const {user, isAuthenticated} = useAuth0()
+    const { user, isAuthenticated } = useAuth0()
     const dispacth = useDispatch()
-    if(isAuthenticated && !isAuthenticatedLocal){
+    if (isAuthenticated && !isAuthenticatedLocal) {
         dispacth({
             type: 'LOGIN_SUCCESS',
             payload: user
@@ -28,11 +29,12 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
 export default function Routes() {
     return (
-        
+
         <Router>
             <Switch>
                 <Route path="/" element={< PrivateRoute component={Board} />} />;
                 <Route path="/login" element={<Login />} />;
+                <Route path="/rooms" element={<Rooms />} />;
             </Switch>
         </Router>
     )

69b286338d7cbfdbc4bf7970b5766634388a1a64
flaviozno <flavio.vezono@ufu.br>
2023-08-15 12:29:22 -0300

    dirname

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import { fileURLToPath } from "url";
 import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express(); // serve up production asset
-app.use(express.static("dist")); // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
+app.use(express.static(path.join(__dirname, "dist"))); // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 }); // if not in production use the port 5000
 
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 console.log("server started on port:", PORT);
 app.listen(PORT);

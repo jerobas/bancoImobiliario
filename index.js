0f2b76dcb20eab9dae4694a31d776d224290dd63
João Vitor Sanches <desenvolvimento15@sifat.com.br>
2023-08-15 12:09:29 -0300

    can we deploy? yes we can

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const app = express(); // serve up production asset
+import express from "express";
+import path from "path";
 
+const app = express(); // serve up production asset
 app.use(express.static("dist")); // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
-const path = require("path");
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));

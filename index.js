be298fa10c43572a673a03ff5ca7de2ca4825576
flaviozno <flavio.vezono@ufu.br>
2023-08-15 12:32:08 -0300

    port

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 }); // if not in production use the port 5000
 
-const PORT = 80;
+const PORT = 5000;
 console.log("server started on port:", PORT);
 app.listen(PORT);
